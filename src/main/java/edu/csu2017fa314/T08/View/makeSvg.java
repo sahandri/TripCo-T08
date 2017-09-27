a179d9edbd8a8bd2ad4cc1d58958bb7bbe207986
mjgaffne <mjgaffne@rams.colostate.edu>
2017-09-27 16:15:02 -0600

    Changed makeSvg.java to output a .svg file showing the Colorado borders

--- src/main/java/edu/csu2017fa314/T08/View/makeSvg.java
+++ src/main/java/edu/csu2017fa314/T08/View/makeSvg.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class makeSvg {
 	public static void main(String[] args) {
@@ -7,6 +10,7 @@ public class makeSvg {
 		//constructLineSVG calls coordinate converter from Lat/Long to local coordinates in file
 		//constructSVG inserts line for each leg of trip and outputs svg file with total trip
 		System.out.println(constructLineSVG());	
+		createTripFile();
 	}
 	
 	public static String xmlDec() {
@@ -41,6 +45,10 @@ public class makeSvg {
 		return "<line id=\"" + id + "\" y2=\"" + y2 + "\" x2=\"" + x2 + "\" y1=\"" + y1 + "\" x1=\"" + x1 + "\" stroke-width=\"" + strokeWidth + "\" stroke=\""+ color + "\"/>";
 	}
 	
+	public static String drawStartPoint(int cx, int cy) {
+		return "<circle cx=\"" + cx + "\" cy=\"" + cy + "\" r=\"10\" stroke=\"red\" fill=\"transparent\" stroke-width=\"20\"/>";
+	}
+	
 	public static String constructLineSVG() {
 		String svgFile = "";
 		for(int i = 0; i <= 4; i++) {
@@ -54,4 +62,31 @@ public class makeSvg {
 		return svgFile;
 	}
 	
+	public static void createTripFile() {
+		try{
+			//Create and open a writer for a new .svg file
+			BufferedWriter ob = new BufferedWriter(new FileWriter("shortestTrip.svg"));
+			//Writing to file...
+	        ob.write(xmlDec());
+	        ob.write("\n" + svgSizeTag());
+	        ob.write("\n" + gTag1());
+	        ob.write("\n" + titleTag("Title"));
+	        ob.write(commentTag("Comments go Here!"));
+	        //Draw State Borders
+	        ob.write("\n" + drawLine("north", 50, 50, 1230, 50, 5, "#000000"));
+	        ob.write("\n" + drawLine("north", 1230, 50, 1230, 974, 5, "#000050"));
+	        ob.write("\n" + drawLine("north", 1230, 974, 50, 974, 5, "#000100"));
+	        ob.write("\n" + drawLine("north", 50, 974, 50, 50, 5, "#000150"));
+	        //Draw trip
+	        ob.write("\n" + drawStartPoint(500, 500));
+			for(int i = 0; i <= 4; i++) {
+				ob.write("\n" + drawLine("north", 1, 1, 1000, 1000, 5, "#000000"));
+			}
+			ob.write("\n" + gTag2());
+			ob.write("\n" + svgTag());
+	        ob.close();
+		} catch (IOException e) {
+		   // do something
+		}
+	}
 }
