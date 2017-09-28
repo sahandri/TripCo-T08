40e006987fdbf0c4ece33bbb6d8ee908154055dd
Adam Watson <adamcwatson57@gmail.com>
2017-09-28 14:51:14 -0400

    Oopsie

--- src/main/java/edu/csu2017fa314/T08/Model/ShortestTrip.java
+++ src/main/java/edu/csu2017fa314/T08/Model/ShortestTrip.java
@@ -11,10 +11,10 @@ public class ShortestTrip {
 
     public static ArrayList<String> getShortestTrip() {
 
+        int tripLength = -1;
         if(stops.size() == 0) {
             buildDistLookUp();
 
-            int tripLength = -1;
             for(int i = 0; i < Destination.getTotal(); i++) {
                 // Get the shortest possible trip from i
                 int currTrip = shortestTripLength(i);
