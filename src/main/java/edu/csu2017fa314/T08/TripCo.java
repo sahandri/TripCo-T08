886fe7133be0346c13409d433ee1b0729e061079
a-watson <adamcwatson57@gmail.com>
2017-10-17 13:01:28 -0600

    Cleaned up the code to improve our test coverage.

--- src/main/java/edu/csu2017fa314/T08/TripCo.java
+++ src/main/java/edu/csu2017fa314/T08/TripCo.java
@@ -2,36 +2,11 @@ package edu.csu2017fa314.T08;
 
 import java.io.IOException;
 import edu.csu2017fa314.T08.Model.Destination;
-import edu.csu2017fa314.T08.Model.Model;
 import edu.csu2017fa314.T08.View.Itinerary;
 import edu.csu2017fa314.T08.View.makeSvg;
-import edu.csu2017fa314.T08.View.CSV;
+
 public class TripCo
 {
-
-    private String name = "";
-
-    public String getName()
-    {
-        return name;
-    }
-
-    String getMessage()
-    {
-        if (name.equals(""))
-        {
-            return "Hello!";
-        }
-        else
-        {
-            return "Hello " + name + "!";
-        }
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to TripCo");
